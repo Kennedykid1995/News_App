0effb8fd1f4c46a5fb2f0d45be8d4e0c4f583c9e
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2019-03-12 18:28:27 -0500

    starting a news app built with redux and an express nodejs backend

---
+++ frontend /src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Grid from '@material-ui/core/Grid';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Grid container spacing={24}>
+
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default App;
