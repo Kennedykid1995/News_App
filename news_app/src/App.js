2295d1ced5f9e600e2995671a66c31ac70e8e3be
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2019-03-14 16:31:27 -0500

    pushing backend along with the installed API

--- news_app/src/App.js
+++ news_app/src/App.js
@@ -5,7 +5,10 @@ import Home from "./components/pages/home";
 import Article from "./components/pages/article"
 import NavBar from "./components/navigation/navbar";
 import { connect } from 'react-redux';
+import NEWS_API from './config_keys';
 
+
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API}`; 
 
 class App extends Component {
   render() {
@@ -18,5 +21,4 @@ class App extends Component {
     );
   }
 }
-
 export default withRouter(App);
--- news_app/src/index.js
+++ news_app/src/index.js
@@ -7,7 +7,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
 import reducers from './reducers/reducer';
 import thunk from 'redux-thunk';
-
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(  
